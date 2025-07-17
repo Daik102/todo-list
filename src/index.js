117c174939b513fca001072aa768fa200c90f79a
Daik <fruit@kd6.so-net.ne.jp>
2025-07-17 16:54:08 +0900

    Start the project

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
 import './home.css';
-import './menu.css';
-import './contact.css';
-import { renderHome } from './home.js';
-import { renderMenu } from './menu.js';
-import { renderContact } from './contact.js';
+import { } from './home.js';
 
-function switchPage() {
-  const btns = document.querySelectorAll('.btn');
-  const content = document.getElementById('content');
-
-  btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      content.innerHTML = '';
-
-      if (e.target.classList.contains('home')) {
-        renderHome();
-      } else if (e.target.classList.contains('menu')) {
-        renderMenu();
-      } else if (e.target.classList.contains('contact')) {
-        renderContact();
-      }
-    });
-  });
-}
-
-switchPage();
-renderHome();
